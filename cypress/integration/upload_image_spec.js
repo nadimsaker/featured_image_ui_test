e4e1c8c33ecb38d7ff5adfad6a1c92b6c5aeeea2
Nadim Saker <nadimsaker@gmail.com>
2022-03-22 02:42:58 +0600

    The user uploads an image from their local machine to the library

--- cypress/integration/upload_image_spec.js
+++ cypress/integration/upload_image_spec.js
@@ -8,4 +8,15 @@ describe('Users should be able to use upload images for use in articles.', () =>
         cy.wait(5000)
         cy.get('.nc-library').click()
     })
+
+    it.only('The user uploads an image from their local machine to the library', () => {
+        cy.visit("https://app.welcomesoftware.com/")
+        cy.get('.form-control').type("nadimsaker@gmail.com")
+        cy.get('#submit-btn').click()
+        cy.get('.form-control').type('G*mTy5!cWj4C&9')
+        cy.get('#submit-btn').click()
+        cy.wait(5000)
+        cy.get('.nc-library').click()
+        
+    })
   })
