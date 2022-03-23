0341b60b009aa680204b4d39b1cd5be4fc45c1dd
Nadim Saker <nadimsaker@gmail.com>
2022-03-24 04:10:10 +0600

    The user completes the task

--- cypress/integration/upload_image_spec.js
+++ cypress/integration/upload_image_spec.js
@@ -5,24 +5,15 @@ import 'cypress-iframe';
 
 describe('Users should be able to use upload images for use in articles.', () => {
     beforeEach(() => {
-
         cy.visit("/")
         cy.login("nadimsaker@gmail.com",'G*mTy5!cWj4C&9')
-      
       })
-    it('The user navigates to the library', () => {
-        cy.visit("https://app.welcomesoftware.com/")
-        cy.get('.form-control').type("nadimsaker@gmail.com")
-        cy.get('#submit-btn').click()
-        cy.get('.form-control').type('G*mTy5!cWj4C&9')
-        cy.get('#submit-btn').click()
-        cy.wait(5000)
-        cy.get('.nc-library').click()
+      
+      it('The user navigates to the library', () => {
+        cy.navigateToLibrary()
     })
 
     it('The user uploads an image from their local machine to the library', () => {
-        cy.visit("/")
-        cy.login("nadimsaker@gmail.com",'G*mTy5!cWj4C&9')
         cy.navigateToLibrary()
         cy.get('.display-flex > .ndl-Button--primary > .ndl-Button-label').click()
         cy.wait(5000)
@@ -31,7 +22,7 @@ describe('Users should be able to use upload images for use in articles.', () =>
            fileName: 'sundarban-day.jpg',
            filePath: "\\images\\sundarban-day.jpg",
            subjectType: 'drag-n-drop' 
-       });
+        });
        cy.wait(5000)
        cy.get('.cl-AssetUpdateForm-inputs > :nth-child(1) > .ndl-Input > .ndl-Input-field-wrapper > .ndl-Input-field > .ndl-Input-input').click().type('Deer')
        cy.get('.cl-AssetUpdateForm-inputs > :nth-child(2) > .ndl-Input > .ndl-Input-field-wrapper > .ndl-Input-field > .ndl-Input-input').click().type('Sundarban')
@@ -45,8 +36,7 @@ describe('Users should be able to use upload images for use in articles.', () =>
     })
 
     it('The user opens the uploaded image in the image editor and resizes the image and Saves the resized image', () => {
-        cy.visit("/")
-        cy.login("nadimsaker@gmail.com",'G*mTy5!cWj4C&9')
+       
         cy.navigateToLibrary()
         cy.get('.thumbnail').first().dblclick()
         cy.get(':nth-child(4) > .ndl-Button-label').click()
@@ -59,8 +49,7 @@ describe('Users should be able to use upload images for use in articles.', () =>
 
     
     it('The user creates a new task', () => {
-        cy.visit("/")
-        cy.login("nadimsaker@gmail.com",'G*mTy5!cWj4C&9')
+       
         cy.get('#create-new > .ndl-Dropdown > .ndl-Button').click()
         cy.get(':nth-child(2) > .ndl-Dropdown-option > .ndl-Option-label > a').click()
         cy.get('.tsk-TaskForm > :nth-child(1) > :nth-child(1) > .ndl-FormControl > .ndl-Input > .ndl-Input-field-wrapper > .ndl-Input-field > .ndl-Input-input').clear().type('Resize the attached image')
@@ -76,7 +65,7 @@ describe('Users should be able to use upload images for use in articles.', () =>
     })
 
     
-    it.only('The user adds a new article to the newly created task', () => {
+    it('The user adds a new article to the newly created task', () => {
 
         cy.get('.nc-plan > .nav-icon-wrapper > svg').click()
         cy.get('.ndl-SearchBar-collapsed > .ndl-FormControl > .ndl-Input > .ndl-Input-field-wrapper > .ndl-Input-field > .ndl-Input-input').click().type('Resize the attached image')
@@ -90,6 +79,25 @@ describe('Users should be able to use upload images for use in articles.', () =>
         cy.iframe('.tox-edit-area__iframe').click().type('This is a test description.')
         cy.get('.art-Header--rightActions > .ndl-Button--primary').click()
     })
+
+    it.only('The user uses the image uploaded to the library in the earlier step as the article’s featured image', () => {
+
+        cy.get('.nc-plan > .nav-icon-wrapper > svg').click()
+        cy.get('.ndl-SearchBar-collapsed > .ndl-FormControl > .ndl-Input > .ndl-Input-field-wrapper > .ndl-Input-field > .ndl-Input-input').click().type('Resize the attached image')
+        cy.wait(3000)
+        cy.get('.ag-row-odd > .ag-cell > .ag-react-container > .ndl-GroupCellWrapper > .ndl-GroupCellWrapper-inner-container > .uni-TitleCell > .uni-TitleCell-titleWrapper > .uni-TitleCell-titleContainer > a > .uni-TitleCell-title').click()
+        cy.get('.left-pane > .ndl-Tabs > :nth-child(2) > .ndl-Tabs-option > .ndl-Option-label').click()
+        cy.get('.content-tab-header-top > :nth-child(1) > .ndl-Dropdown > .ndl-Button > .ndl-Button-label').click()
+        cy.get(':nth-child(4) > .ndl-Dropdown-option > .ndl-Option-label > .ndl-Option-truncatedLabel').click()
+        cy.waitFor('ndl-Checkbox-input')
+        cy.get('.ndl-Checkbox-label').first().check()
+        cy.get('.library-preview-left-buttons-container > .ndl-Button--primary').click()
+
+    })
+
+
+   
+
 
 
   })
